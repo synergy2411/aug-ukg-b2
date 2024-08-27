8514a46822319fe44abdbd5e37ef14b0a4f7b073
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-27 10:50:25 +0530

    validation added

--- employee-service/src/main/java/com/sk/employee_service/controller/EmployeeController.java
+++ employee-service/src/main/java/com/sk/employee_service/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.sk.employee_service.controller;
 import com.sk.employee_service.dto.EmployeeDto;
 import com.sk.employee_service.dto.ResponseDto;
 import com.sk.employee_service.service.IEmployeeService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
@@ -18,7 +22,7 @@ public class EmployeeController {
     private IEmployeeService iEmployeeService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<ResponseDto> createEmployee(@RequestBody @Valid EmployeeDto employeeDto){
         iEmployeeService.createEmployee(employeeDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 new ResponseDto("Created successfully", "201")
@@ -26,7 +30,8 @@ public class EmployeeController {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<EmployeeDto> fetchEmployee(@RequestParam String mobileNumber){
+    public ResponseEntity<EmployeeDto> fetchEmployee(@RequestParam
+                                                         @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should have 10 digits") String mobileNumber){
         EmployeeDto employeeDto = iEmployeeService.fetchEmployee(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(employeeDto);
     }
@@ -39,7 +44,7 @@ public class EmployeeController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateEmployee(@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<ResponseDto> updateEmployee(@RequestBody @Valid EmployeeDto employeeDto){
         boolean isUpdated =  iEmployeeService.updateEmployee(employeeDto);
         if(isUpdated){
             return ResponseEntity.status(HttpStatus.OK).body(
@@ -51,7 +56,8 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteEmployee(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> deleteEmployee(@RequestParam
+                                                          @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should have 10 digits") String mobileNumber){
         boolean isDeleted =  iEmployeeService.deleteEmployee(mobileNumber);
         if(isDeleted){
             return ResponseEntity.status(HttpStatus.OK).body(
--- employee-service/src/main/java/com/sk/employee_service/entity/Employee.java
+++ employee-service/src/main/java/com/sk/employee_service/entity/Employee.java
@@ -1,6 +1,10 @@
 package com.sk.employee_service.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +21,20 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @NotEmpty(message = "Employee name can not be empty")
+    @NotNull(message = "Employee name can not be empty")
     private String name;
+
+    @NotEmpty(message = "Email can not be empty")
+    @NotNull(message = "Email can not be empty")
     private String email;
+
+    @NotEmpty(message = "Mobile number can not be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should have 10 digits")
     private String mobileNumber;
+
+
     private String designation;
     private String department;
 
